118b9d0b2d62bc93dfdcce5db1e6e15c238e2498
disj11 <disj11@naver.com>
2025-02-05 21:05:58 +0900

    상단바에 검색 UI 추가

--- src/layouts/Layout.tsx
+++ src/layouts/Layout.tsx
@@ -1,10 +1,15 @@
 import { DashboardLayout } from "@toolpad/core/DashboardLayout";
 import { PageContainer } from "@toolpad/core/PageContainer";
 import { Outlet } from "react-router";
+import ToolbarActionsSearch from "./ToolbarActionsSearch";
 
 export default function Layout() {
   return (
-    <DashboardLayout>
+    <DashboardLayout
+      slots={{
+        toolbarActions: ToolbarActionsSearch,
+      }}
+    >
       <PageContainer maxWidth="xl">
         <Outlet />
       </PageContainer>
