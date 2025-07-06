7cdc3f25b8e35c986815de464bc4ad6f347e28b3
disj11 <disj11@naver.com>
2025-07-06 18:53:27 +0900

    Add SPA redirect handling for GitHub Pages 404s

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,28 @@
-import { Outlet } from "react-router";
+import { Outlet, useNavigate, useLocation } from "react-router";
+import { useEffect } from "react";
+
+function RedirectHandler() {
+  const navigate = useNavigate();
+  const location = useLocation();
+
+  useEffect(() => {
+    const params = new URLSearchParams(location.search);
+    const redirect = params.get("redirect");
+    if (redirect) {
+      navigate(redirect, { replace: true });
+    }
+  }, [location, navigate]);
+
+  return null;
+}
 
 function App() {
-  return <Outlet />;
+  return (
+    <>
+      <RedirectHandler />
+      <Outlet />
+    </>
+  );
 }
 
 export default App;
